a5052cdedb3215abe340c9807506edc7860de700
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2022-05-01 21:24:42 +0300

    FINISHED

--- src/index.js
+++ src/index.js
@@ -1,42 +1,26 @@
 import qq from './template_of_food.hbs'
-
 import json_template from './menu.json' 
+localStorage.removeItem('randid')
 
-console.log( json_template);
-console.log(qq);
 const a = document.querySelector('#menu')
+json_template.forEach((elem)=>{
+ const template = qq(elem)  
+a.insertAdjacentHTML('beforeend', template)
+})
 
-
-// проблема со строчками 13-14, строки 58-72 не относятся к коду
-
-
-// const template = qq(JSON.parse(json_template))  
-// console.log(template);
 
 
 
 
 // =========================================THEME==========================================================================
 
-
 const btn_switch_theme = document.querySelector('#theme-switch-toggle');
-const theme = {
-        light: 'light-theme',
-        dark: 'dark-theme',
-      };
-
-
-
-
-
-
-
-
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme == "dark") {
   
   document.body.classList.add("dark-theme");
+  btn_switch_theme.checked = true
 }
 
 
@@ -49,27 +33,13 @@ btn_switch_theme.addEventListener("change", function () {
   if (document.body.classList.contains("dark-theme")) {
  
     theme = "dark";
+    
   }
 
   localStorage.setItem("theme", theme);
 });
 
 
-// const currentTheme = localStorage.getItem('theme')
-
-// if(currentTheme == Theme.DARK){
-//     document.body.classList.add('Theme.DARK')
-// }
-
-// btn_switch_theme.addEventListener('change', function(){
-//     document.body.classList.toggle('Theme.DARK')
-//     let theme = 'Theme.LIGHT'
-//     if(document.body.classList.contains('Theme.DARK')){
-//         theme = Theme.DARK
-//     }
-
-//     localStorage.setItem('theme', theme)
-// })
 
 
 
@@ -93,31 +63,3 @@ btn_switch_theme.addEventListener("change", function () {
 
 
 
-
-
-
-
-
-// const myObj = {
-//     title: 'Languages, that i want to know',
-//     arr: ['phyton', 'java script', 'java', 'kotlin', 'c++']
-
-// }
-
-// console.log(template);
-// localStorage.setItem('title', myObj.title)
-// localStorage.setItem('arr',JSON.stringify(myObj.arr))
-// localStorage.removeItem('randid')
-
-
-// const g = JSON.stringify(json_template)
-
-
-// a.insertAdjacentHTML('afterbegin', template)
-// console.log('object');
-// document.addEventListener('DOMContentLoaded',() => {
-// const q = localStorage.getItem('title')
-// const w = localStorage.getItem('arr')
-// const e = JSON.parse(w)
-// console.log(q, e);
-// })
